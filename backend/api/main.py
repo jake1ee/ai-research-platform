4c1b2c5175f13d4af4e418e2592557e7fe192905
TQ <terqin.lee@innovatus.net>
2026-03-04 13:13:51 +0800

    Revert "Configure frontend and backend to run the application locally"
    This reverts commit 66d5a8f325cd7346999cd430996d19ce0f326fd7.

--- backend/api/main.py
+++ backend/api/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # 1. Add CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5000"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
